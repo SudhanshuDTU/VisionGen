2004167b157c8b3a0eb4a9222ae03fb1d0ba25ec
Sudhanshu jha <116909414+SudhanshuDTU@users.noreply.github.com>
2024-03-01 00:55:16 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
     const fetchPosts = async () => {
       setloading(true);
       try {
-        const response = await fetch("http://localhost:4400/api/v1/post", {
+        const response = await fetch("https://visiongen.onrender.com/api/v1/post", {
           method: "GET",
           headers: { "Content-Type": "application/json" },
         });
